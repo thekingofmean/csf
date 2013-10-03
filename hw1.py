8c4679f55f87675622f74d32f558e1087a695bac
edwchr30 <edwchr30@evergreen.edu>
2013-10-03 07:36:12 -0700

    Update hw1.py
    Added print lines so the user can see the processes that are being used, instead of just having a script that spits out some numbers and a true/false statement.

--- hw1.py
+++ hw1.py
@@ -12,6 +12,8 @@
 import math                     # makes the math.sqrt function available
 
 
+### I opted to add print lines to show the use what is going on behind the scenes.
+
 ###
 ### Problem 1
 ###
@@ -22,11 +24,16 @@ print "Problem 1 solution follows:"
 a = 1
 b = -5.86
 c = 8.5408
-
+print "a = 1"
+print "b = -5.86"
+print "c = 8.5408"
 # This is the equation used to find the roots.
 # Notice the slight difference between the two lines.
 firstRoot = (-b +(math.sqrt(b*b-4*a*c)/(2*a)))
 secondRoot = (-b -(math.sqrt(b*b-4*a*c)/(2*a)))
+
+print "We find our first root with (-b +(math.sqrt(b*b-4*a*c)/(2*a)))"
+print "and our second root with (-b -(math.sqrt(b*b-4*a*c)/(2*a)))"
 
 # This will print the solution to our problem
 print 'The first root is:'
@@ -42,10 +49,10 @@ print "Problem 2 solution follows:"
 
 # Here we use "execfile("filename") to add an external file,
 # in this case setting a number of variables at once.
-# I'll print a line so that the user knows something is happening here.
 
 execfile("hw1_test.py")
-print "hw1_test.py imported successfully."
+### I'd like to "CAT" out the contents of hw1_test.py here, but don't know how yet.
+print "hw1_test.py has been imported successfully."
 
 ###
 ### Problem 3
@@ -55,7 +62,9 @@ print "Problem 3 solution follows:"
 
 # This uses the variables defined in hw1_test and prints out the following expression as a string
 
+print "The results for the expression ((a^b)v(~c)^~(dvevf)) are in!"
 print ((a and b) or (not c) and not (d or e or f))
+
 
 
 ###
