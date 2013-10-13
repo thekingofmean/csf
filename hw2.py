112ba4d37f5b5b06a6983bf99964017e913c4a8c
edwchr30 <edwchr30@evergreen.edu>
2013-10-12 23:25:29 -0700

    Create hw2.py

---
+++ hw2.py
@@ -0,0 +1,87 @@
+# Name: Chris Edwards
+# Evergreen Login: edwchr30
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 2
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+
+###
+### Problem 1
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 1 solution follows:"
+
+import hw_test2
+
+x = "hw_test2.n"
+for i in range(hw_test2.n):
+    x = x + i
+print x
+
+
+###
+### Problem 2
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 2 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 3
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 4
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Collaboration
+###
+
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
+
+###
+### Reflection
+###
+
+# ... Write how long this assignment took you, including doing all the readings
+# ... and tutorials linked to from the homework page. Did the readings, tutorials,
+# ... and lecture contain everything you needed to complete this assignment?
