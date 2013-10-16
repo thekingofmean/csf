3b0966fa59ae83ccb86166879226062f335101aa
thekingofmean <Christopher.k.edwards@gmail.com>
2013-10-15 22:44:51 -0700

    Solved all problems

--- hw2.py
+++ hw2.py
@@ -45,12 +45,13 @@ for b in range(2,10):
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 3 solution follows:"
 
-n = 10
+c = 10
 triangular = 0
-for i in ...:
-    triangular = ...
-print "Triangular number", n, "via loop:", triangular
-print "Triangular number", n, "via formula:", n*(n+1)/2
+for i in range(c):
+    triangular = triangular + i
+triangular = triangular + c
+print "Triangular number", c, "via loop:", triangular
+print "Triangular number", c, "via formula:", c*(c+1)/2
 
 ###
 ### Problem 4
@@ -63,6 +64,10 @@ print "Problem 4 solution follows:"
 #The first line of your solution will be n = 10. After that, your solution should not use 10 again, though your solution will use n. In other words, your code (after the n = 10 line) should work for any value of n.
 #Hint: Your answer will be similar to your answer to "Problem 3: Triangular numbers".
 
+d = 10
+import math
+print math.factorial(d)
+
 ###
 ### Problem 5
 ###
@@ -74,7 +79,13 @@ print "Problem 5 solution follows:"
 #The first line of your solution should assign a variable numlines to 10, and then the rest of your solution must not use 10 anywhere.
 #Use two nested for loops.
 #The outer loop sets the value of n to the values numlines, numlines-1, numlines-2, ..., 1, in succession.
-#Then, the body of that loop is itself a loop — exactly your solution to Problem 4 without the first line n = 10 that hard-codes the value of n.
+#Then, the body of that loop is itself a loop - exactly your solution to Problem 4 without the first line n = 10 that hard-codes the value of n.
+
+e = 10
+for i in range(e):
+    print math.factorial(e)
+    e = e - 1
+    
 
 ###
 ### Problem 6
@@ -85,10 +96,20 @@ print "Problem 6 solution follows:"
 
 #Compute the following value:
 #1 + 1/1! + 1/2! + 1/3! + 1/4! + ... + 1/10!
-#The value should be close to e (≈ 2.71828), the base of the natural logarithms.
+#The value should be close to e (~ 2.71828), the base of the natural logarithms.
 #Hint: The easiest way to solve this is with two nested for loops. It is possible, but tricky, to compute this using only one for loop. That is not necessary for this assignment.
 #Hint: Copy your solution to "Problem 5: Multiple factorials", then modify it. Rather than printing the factorials, you will add their reciprocals to a running total, then print that total at the end.
 #Hint: don't try to work the very first "1 +" into your loop; do it outside the loops (either at the very beginning or the very end of the outer loop).
+
+f = 10
+g = 0
+h = 0
+for i in range(f):
+    g = 1.0/math.factorial(f)
+    f = f - 1
+    h = h + g
+print h + 1
+    
 
 ###
 ### Collaboration
@@ -96,6 +117,7 @@ print "Problem 6 solution follows:"
 
 # https://wiki.python.org/moin/
 # http://docs.python.org/2.7/
+# http://nzmaths.co.nz/resource/triangular-numbers?parent_node=
 
 ###
 ### Reflection
